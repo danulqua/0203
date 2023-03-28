74739b210f2b587ccdf5b019c6bb4d58be18f60d
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-03-28 22:43:40 +0300

    feat: make bio responsive

--- src/components/ProfileBio.vue
+++ src/components/ProfileBio.vue
@@ -1,11 +1,11 @@
 <template>
   <header class="relative">
-    <div class="ml-24">
-      <div class="pl-12 font-bold">{{ fullName }}</div>
-      <div class="mt-1 pl-12 text-xs text-brandgray-400">{{ position }}</div>
-      <div class="mt-2 rounded border border-brandgray-200 bg-brandyellow-300 py-2 pl-12 pr-2">
+    <div class="xs:ml-24">
+      <div class="pl-14 font-bold xs:pl-12">{{ fullName }}</div>
+      <div class="mt-1 pl-14 text-xs text-brandgray-400 xs:pl-12">{{ position }}</div>
+      <div class="mt-2 rounded border border-brandgray-200 bg-brandyellow-300 px-4 py-2 xs:pl-12">
         <pre class="font-sans text-sm">{{ description }}</pre>
-        <div class="absolute left-0 top-0 h-32 w-32 overflow-hidden rounded-full">
+        <div class="absolute left-0 top-0 h-11 w-11 overflow-hidden rounded-full xs:h-32 xs:w-32">
           <img :src="avatar" alt="user avatar" class="h-full w-full object-cover" />
         </div>
       </div>
