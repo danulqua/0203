1af117a16f1a6a76849ac908522369d62d131ba8
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-03-28 20:50:37 +0300

    feat: add brand colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,31 @@ module.exports = {
       fontFamily: {
         sans: ['Arial', ...defaultTheme.fontFamily.sans],
         'pt-sans': ['PT Sans', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        brandyellow: {
+          300: '#FFFBC8',
+          900: '#FDD639'
+        },
+        brandgreen: {
+          700: '#B1E19B'
+        },
+        brandblue: {
+          100: '#F2FBFF',
+          700: '#ACE2F8',
+          800: '#73B4D5',
+          900: '#005DA1'
+        },
+        brandgray: {
+          50: '#33333320',
+          100: '#F2F2F2',
+          200: '#DADADA',
+          300: '#C4CBCF',
+          400: '#808080'
+        },
+        branddark: {
+          900: '#333333'
+        }
       }
     }
   },
