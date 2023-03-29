91e4084cd53e4d9ebae83e4bd99392db9404d4e3
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-03-29 08:30:31 +0300

    fix: prevent adding empty review

--- src/components/AddReviewForm.vue
+++ src/components/AddReviewForm.vue
@@ -3,7 +3,7 @@
     <textarea
       id="text"
       ref="textareaRef"
-      v-model="reviewText"
+      v-model.trim="reviewText"
       name="text"
       class="h-16 w-full resize-none scroll-py-2 overflow-y-auto rounded border border-black px-5 py-2 text-sm outline-none ring-brandyellow-900 ring-offset-2 focus:ring-2"
       @keydown.meta.enter="addReview(reviewText)"
@@ -20,12 +20,20 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useToast, POSITION } from 'vue-toastification';
 
 const reviewText = ref('');
 const textareaRef = ref<HTMLTextAreaElement | null>(null);
+const toast = useToast();
 
 const emit = defineEmits(['add-review']);
 const addReview = (text: string) => {
+  if (!text) {
+    return toast.error('Error! Please enter a review text.', {
+      position: POSITION.BOTTOM_RIGHT
+    });
+  }
+
   emit('add-review', text);
   reviewText.value = '';
   textareaRef.value?.blur();
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+import Toast from 'vue-toastification';
 import App from './App.vue';
 
+import 'vue-toastification/dist/index.css';
 import './assets/main.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(Toast);
+
+app.mount('#app');
